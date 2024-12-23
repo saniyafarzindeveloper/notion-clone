731759334ebfba837c522ae841bdc4eec6af3bdd
saniyafarzindeveloper <developerfarzin@gmail.com>
2024-12-23 20:46:50 +0530

    debugging

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-
 import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import React from "react";
 
 export default function Header() {
   const { user } = useUser();
-  return (
-    <div className="flex items-center justify-between p-5 bg-slate-800">
-      {user && (
-        <h1>
-          {user?.firstName}
-          {`'s`} Space
-        </h1>
-      )}
 
+  return (
+    <div className="flex items-center justify-between px-4 py-2 bg-red-800">
+      <h1 className="text-xl font-bold">
+        {user?.firstName}
+        {`'s`} Space
+      </h1>
       <div>
         <SignedOut>
           <SignInButton />
